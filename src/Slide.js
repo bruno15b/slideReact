156dcca462a730a4fb60f45b5d809850f04ea6fa
Bruno Machado <brunomachado3818@gmail.com>
2022-12-07 16:59:59 -0300

    adicionado navegação ao deslize mouse e correções

--- src/Slide.js
+++ src/Slide.js
@@ -3,9 +3,13 @@ import styles from "./Slide.module.css";
 import debounce from "./debounce";
 
 const Slide = ({ slides }) => {
+  const slidesWrapper = React.useRef([]);
   const slidesSelec = React.useRef([]);
   const [slideActive, setSlideActive] = React.useState(0);
   const [position, setPosition] = React.useState();
+  const [initialClick, setInitalClick] = React.useState(0);
+  const [travelMouseDist, setTravelMouseDist] = React.useState(0);
+  const [clickEnable, setClickEnable] = React.useState(true);
   const [randomColors] = React.useState(() => {
     return slides.reduce((arrayAcc) => {
       const newItem =
@@ -33,14 +37,14 @@ const Slide = ({ slides }) => {
     }
   }
 
-  function resizeSlide() {
+  const returnSlideOrigin = debounce(() => {
     setPosition(-slidesSelec.current[slideActive].offsetLeft + (window.innerWidth - slidesSelec.current[slideActive].offsetWidth) / 2);
-  }
+  }, 100);
 
   React.useEffect(() => {
-    window.addEventListener("resize", debounce(resizeSlide, 100));
-    return () => window.removeEventListener("resize", resizeSlide);
-  });
+    window.addEventListener("resize", returnSlideOrigin);
+    return () => window.removeEventListener("resize", returnSlideOrigin);
+  }, [slideActive]);
 
   function clickOnSlides(event) {
     if (event.clientX > window.innerWidth / 2 && slideActive < slides.length - 1) {
@@ -50,9 +54,80 @@ const Slide = ({ slides }) => {
     }
   }
 
+  const mouseOnMove = React.useCallback(
+    (event) => {
+      setTravelMouseDist(initialClick[0] - event.clientX);
+      setPosition(initialClick[1] - (initialClick[0] - event.clientX));
+    },
+    [initialClick]
+  );
+
+  React.useEffect(() => {
+    const target = slidesWrapper.current;
+    if (initialClick) {
+      target.addEventListener("mousemove", mouseOnMove);
+    } else {
+      target.removeEventListener("mousemove", mouseOnMove);
+    }
+    return () => target.removeEventListener("mousemove", mouseOnMove);
+  }, [initialClick, mouseOnMove]);
+
+  function mouseDownOnSlides(event) {
+    setInitalClick([event.clientX, position]);
+  }
+
+  function slipPrevSlide() {
+    setClickEnable(false);
+    setSlideActive(slideActive - 1);
+    setTravelMouseDist(0);
+  }
+
+  function slipNextSlide() {
+    setClickEnable(false);
+    setSlideActive(slideActive + 1);
+    setTravelMouseDist(0);
+  }
+
+  function dontSlipPrevSlideReturn() {
+    setClickEnable(false);
+    returnSlideOrigin();
+    setEnableClickWithtimer();
+    setTravelMouseDist(0);
+  }
+
+  function dontSlipNextSlideReturn() {
+    setClickEnable(false);
+    returnSlideOrigin();
+    setEnableClickWithtimer();
+    setTravelMouseDist(0);
+  }
+
+  function mouseUpOnSlides() {
+    setInitalClick(null);
+    if (travelMouseDist > 50 && slideActive < slides.length - 1) {
+      slipNextSlide();
+    } else if (travelMouseDist < -50 && slideActive > 0) {
+      slipPrevSlide();
+    } else if (travelMouseDist < -50 && slideActive === 0) {
+      dontSlipPrevSlideReturn();
+    } else if (travelMouseDist > 50 && slideActive === slides.length - 1) {
+      dontSlipNextSlideReturn();
+    }
+  }
+
+  function setEnableClickWithtimer() {
+    setTimeout(() => {
+      setClickEnable(true);
+    }, 500);
+  }
+
+  React.useEffect(() => {
+    setClickEnable(true);
+  }, [slideActive]);
+
   return (
     <>
-      <section onClick={clickOnSlides} className={styles.container}>
+      <section ref={slidesWrapper} onMouseDown={mouseDownOnSlides} onClick={clickEnable ? clickOnSlides : () => {}} onMouseUp={mouseUpOnSlides} className={styles.container}>
         <div style={{ transform: `translateX(${position}px)` }} className={styles.content}>
           {slides.map((slide, index) => (
             <div
