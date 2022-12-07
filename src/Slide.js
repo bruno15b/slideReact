fd7b0f2efa6655d4dfb453656c58b51dbd76b8c6
Bruno Machado <brunomachado3818@gmail.com>
2022-12-06 23:50:55 -0300

    adicionado funcionalidades de auto-ajuste ao resizing e navegação ao clique em tela

--- src/Slide.js
+++ src/Slide.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Slide.module.css";
 
 const Slide = ({ slides }) => {
-  const item = React.useRef();
+  const corpoSlide = React.useRef();
+  const itens = React.useRef([]);
   const [ativo, setAtivo] = React.useState(0);
-  const [position, setPosition] = React.useState(0);
+  const [position, setPosition] = React.useState();
   const [cores] = React.useState(() => {
     return slides.reduce((arrayAcc) => {
       const newItem =
@@ -17,8 +18,10 @@ const Slide = ({ slides }) => {
   });
 
   React.useEffect(() => {
-    const { width } = item.current.getBoundingClientRect();
+    const { width } = corpoSlide.current.getBoundingClientRect();
     setPosition(width * ativo);
+    const margin = +getComputedStyle(itens.current[ativo]).marginLeft.replace("px", "");
+    setPosition(-itens.current[ativo].offsetLeft + margin + (window.innerWidth - itens.current[ativo].offsetWidth - 2 * margin) / 2);
   }, [ativo]);
 
   function slidePrev() {
@@ -32,21 +35,51 @@ const Slide = ({ slides }) => {
       setAtivo(ativo + 1);
     }
   }
+  function downClick(event) {
+    if (event.clientX > window.innerWidth / 2 && ativo < slides.length - 1) {
+      setAtivo(ativo + 1);
+    } else if (event.clientX <= window.innerWidth / 2 && ativo > 0) {
+      setAtivo(ativo - 1);
+    }
+  }
+
+  function resizeSlide() {
+    const margin = +getComputedStyle(itens.current[ativo]).marginLeft.replace("px", "");
+    setPosition(
+      itens.current[ativo].offsetLeft -
+        margin +
+        (window.innerWidth - (itens.current[ativo].offsetWidth + 2 * +getComputedStyle(itens.current[ativo]).marginLeft.replace("px", ""))) / 2
+    );
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeSlide);
+    return () => window.removeEventListener("resize", resizeSlide);
+  });
 
   return (
-    <section className={styles.container}>
-      <div ref={item} style={{ transform: `translateX(${-position}px)` }} className={styles.content}>
-        {slides.map((slide, index) => (
-          <div style={{ background: cores[index] }} key={index} className={styles.item}>
-            {slide.text}
-          </div>
-        ))}
-      </div>
+    <>
+      <section onMouseDown={downClick} className={styles.container}>
+        <div ref={corpoSlide} style={{ transform: `translateX(${position}px)` }} className={styles.content}>
+          {slides.map((slide, index) => (
+            <div
+              ref={(element) => {
+                itens.current[index] = element;
+              }}
+              style={{ background: cores[index] }}
+              key={index}
+              className={styles.item}
+            >
+              {slide.text}
+            </div>
+          ))}
+        </div>
+      </section>
       <nav className={styles.nav}>
         <button onClick={slidePrev}>Anterior</button>
         <button onClick={slideNext}>Próximo</button>
       </nav>
-    </section>
+    </>
   );
 };
 
